a3538cf3b6d1a5112d707cb9310d7855a56b1da7
nirtendra <nirtendrayadav12@gmail.com>
2024-01-21 19:02:08 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  let thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+}
+
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
